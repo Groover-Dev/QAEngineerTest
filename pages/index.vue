158705dfb655f7d15b4a1c356225b12847b8318e
Matthew Smith <musicsmith4@gmail.com>
2021-10-21 22:22:40 -0400

    init commit

---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1 class="font-bold text-5xl">Hello world</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "@vue/composition-api";
+
+export default defineComponent({});
+</script>
